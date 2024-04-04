9dd6c8d2edb280f2a1a4e0a19cd2be21240c119d
bkhazaal <thajukez101@gmail.com>
2024-04-04 10:34:18 -0700

    yea

--- eventemitter.js
+++ eventemitter.js
@@ -1,11 +1,19 @@
 const { EventEmitter } = require('events');
 const eventEmitter = new EventEmitter();
 
-eventEmitter.on('yuh', () => {
+function Counter() {
+let counter = 1;
+eventEmitter.on('test', () => {
+    if (counter < 1000) { 
+            console.log(counter);
+            counter++;
+            eventEmitter.emit('test');
 
-    console.log('yea')
+    }
 });
+}
 
-eventEmitter.emit('yuh');
-eventEmitter.emit('yuh');
+Counter();
+eventEmitter.emit('test');
 
+module.exports = Counter;
--- index.js
+++ index.js
@@ -1,17 +1,5 @@
 const express = require('express');
-const { readFile } = require('fs');
 const app = express();
-
-app.get('/', (req, res) => {
-    readFile('index.html', 'utf8', (err, html) => {
-        if (err) {
-            console.errror('Error reading file:', err);
-            res.status(404).send('Internal Server Error');
-            return;
-        }
-        res.send(html);
-    })
-})
 
 app.listen(3000, () => {
     console.log('server running on port 3000');
