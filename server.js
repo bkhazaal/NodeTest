d05b93a79a4df3413a29471a31db26992454b599
bkhazaal <thajukez101@gmail.com>
2024-04-06 15:34:31 -0700

    yuh

--- server.js
+++ server.js
@@ -7,28 +7,20 @@ const mongodb = require('mongodb');
 
 app.get('/server1', (req, res) => {
     res.send('Server Test1')
-})
+});
 
 app.get('/server2', (req, res) => {
     res.send('Server Test 2')
-})
+});
 
 app.get('/server3', (req, res) => {
     res.send('Server Test 3')
-})
+});
 
 app.get('/server4', (req, res) => {
     res.send('Server Test 4')
-})
+});
 
 app.listen(3000, () => {
     console.log('Running on port 3000')
-})
-
-const User = mongoose.model(
-    "User1",
-    new Schema({
-        username: { type: String, required: true },
-        password: { type: String, required: true }
-    })
-);
+});
